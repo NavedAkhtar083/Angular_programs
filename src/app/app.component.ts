2bb110a3bbcd607c6b560c56e6fe81c4488ad975
NavedAkhtar083 <navedkhan9410671292@gmail.com>
2024-01-16 22:13:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { preserveWhitespacesDefault } from '@angular/compiler';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:' <div *ngIf="chkif">this is ngIf  example</div> <button (click)="btnclick()">click me </button> <p [ngStyle]="{color:blue}"> welcpome to the shahasoft</p>',
+  styleUrls: ['./app.component.css'],
+  preserveWhitespaces:true
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'shaasoft channel';
+  data='helo this is angular';
+  chkif=true;
+  isValid:boolean=true;
+blue: any;
+
+btnclick(){
+  alert('you cliked me ');
+
+}
+changevalue(valid:boolean){
+  this.isValid=valid;
+}
+
+@HostListener('click') onClick(){
+  alert("clicked");
+
+}
 }
